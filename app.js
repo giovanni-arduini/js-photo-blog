e38449a042a18bc71db8824384ce5dc592ecbad5
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-06 15:45:00 +0100

    ADD  working function

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const rowElement = document.getElementById("row");
+
 function photosFromAlbum() {
   axios
     .get("https://jsonplaceholder.typicode.com/photos?_limit=6")
@@ -6,7 +8,24 @@ function photosFromAlbum() {
       console.log(album);
 
       album.forEach((photoElement) => {
-        const { title, thumbnailUrl } = photoElement;
+        const { title, url } = photoElement;
+        rowElement.innerHTML += `<div class="col mt-5">
+            <div class="card mx-auto rounded-0" style="width: 18rem">
+              <div class="pin">
+                <img src="./img/pin.svg" alt="" />
+              </div>
+              <img
+                src="${url}"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  ${title}
+                </p>
+              </div>
+            </div>
+          </div>`;
       });
     })
     .catch((err) => {
