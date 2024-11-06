85cd05b7008c255947037d00ea796461f99fa5b0
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-06 15:36:56 +0100

    ADD function and forEach

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-axios
-  .get("https://jsonplaceholder.typicode.com/photos?_limit=6")
-  .then((res) => {
-    const album = res.data;
-    console.log(album);
-  })
-  .catch((err) => {
-    console.log("Qualcosa è andato storto!");
-  });
+function photosFromAlbum() {
+  axios
+    .get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+    .then((res) => {
+      const album = res.data;
+      console.log(album);
+
+      album.forEach((photoElement) => {
+        const { title, thumbnailUrl } = photoElement;
+      });
+    })
+    .catch((err) => {
+      console.log("Qualcosa è andato storto!");
+    });
+}
+
+photosFromAlbum();
